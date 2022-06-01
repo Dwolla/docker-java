bfea745e856fbf861da1f4d6a22a8214571c6994
Lauren Gorski <lgorski@dwolla.com>
2022-06-01 15:42:04 -0500

    import docker image target to snyk before activating project

--- scripts/activate-snyk-project.sh
+++ scripts/activate-snyk-project.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-SNYK_TOKEN=$1
-IMAGE_ID=$2
-TAG=$3
-COMMIT_HASH=$4
+IMAGE_ID=$1
+TAG=$2
+COMMIT_HASH=$3
 TAG_WITH_COMMIT_HASH="dwolla/$IMAGE_ID:$TAG-$COMMIT_HASH"
+
+function get_integrations() {
+     curl \
+    --silent \
+    -H "Authorization: token $SNYK_TOKEN" \
+    https://snyk.io/api/v1/org/dwolla/integrations   
+}
 
 function get_snyk_projects() {
     curl \
@@ -23,14 +29,35 @@ function activate_snyk_project() {
     > /dev/null
 }
 
-echo "Looking Snyk project with name $TAG_WITH_COMMIT_HASH..."
-SNYK_PROJECT_ID=$(get_snyk_projects | jq -c -r --arg IMAGE_NAME "$TAG_WITH_COMMIT_HASH" '.projects[] | select( .name==$IMAGE_NAME ).id')
 
-if [ -z "$SNYK_PROJECT_ID" ]
-then
-echo "No snyk project found for $TAG_WITH_COMMIT_HASH."
+function import_snyk_project() {
+    INTEGRATION_ID=$1
+    curl \
+    -X POST \
+    --silent \
+    https://snyk.io/api/v1/org/dwolla/integrations/"$INTEGRATION_ID"/import \
+    -H "Authorization: token $SNYK_TOKEN" -H "Content-Type: application/json" \
+    -d '{"target": {"name": "'"$TAG_WITH_COMMIT_HASH"'"}}' \
+    > /dev/null
+}
+
+DOCKERHUB_INTEGRATION_ID=$(get_integrations | jq -r '.["docker-hub"]')
+import_snyk_project "$DOCKERHUB_INTEGRATION_ID"
+
+echo "Looking for Snyk project with name $TAG_WITH_COMMIT_HASH..."
+for (( counter=1; counter<=10; counter++ ))
+do  
+    SNYK_PROJECT_ID=$(get_snyk_projects | jq -c -r --arg IMAGE_NAME "$TAG_WITH_COMMIT_HASH" '.projects[] | select( .name==$IMAGE_NAME ).id')
+    if [ -n "$SNYK_PROJECT_ID" ]; then
+    activate_snyk_project "$SNYK_PROJECT_ID"
+    echo "Successfully activated Snyk monitoring for $TAG_WITH_COMMIT_HASH with project id $SNYK_PROJECT_ID."
+    exit 0
+    fi
+    if [ "$counter" -lt 10 ]; then
+    echo "Not found: sleeping 30s then retrying..."
+    sleep 30
+    fi
+done
+
+echo "No snyk project found for $TAG_WITH_COMMIT_HASH, import job may have failed."
 exit 99
-else
-activate_snyk_project "$SNYK_PROJECT_ID"
-echo "Successfully turned on Snyk monitoring for $TAG_WITH_COMMIT_HASH with project id $SNYK_PROJECT_ID."
-fi
